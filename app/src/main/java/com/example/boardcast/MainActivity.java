1dfed47c2d65f967b5eb1021538128feb9262e93
Estella0124 <sunmiding@snnu.edu.cn>
2023-11-16 09:14:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/boardcast/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.boardcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /* AirplanModeReciever airplanModeReciever = new AirplanModeReciever();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("android.intent.action.AIRPLANE_MODE");
+
+        registerReceiver(airplanModeReciever, intentFilter);*/
+        Button button =(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.Boardcast.LOCAL_BOARDCAST");
+               intent.setComponent(new ComponentName("com.example.boardcast",
+                       "com.example.boardcast.MyReceiver"));
+                sendBroadcast(intent, null);
+            }
+        });
+    }
+
+    class AirplanModeReciever extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context,"飞行模式", Toast.LENGTH_LONG).show();
+        }
+    }
+}

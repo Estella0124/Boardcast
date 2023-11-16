a9a6e4b9f2fcad7eeaf05ad439456dbf6f493aad
Estella0124 <sunmiding@snnu.edu.cn>
2023-11-16 09:33:40 +0800

    Network is available

--- app/src/main/java/com/example/boardcast/MainActivity.java
+++ app/src/main/java/com/example/boardcast/MainActivity.java
@@ -7,18 +7,25 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.Network;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
        /* AirplanModeReciever airplanModeReciever = new AirplanModeReciever();
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("android.intent.action.AIRPLANE_MODE");
@@ -43,4 +50,20 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(context,"飞行模式", Toast.LENGTH_LONG).show();
         }
     }
+
+    class NetworkChangeReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectivityManager = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+            Network networkInfo= connectivityManager.getActiveNetwork();
+            if(networkInfo != null){
+                Toast.makeText(context,"network is available",Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(context,"network is not available",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
